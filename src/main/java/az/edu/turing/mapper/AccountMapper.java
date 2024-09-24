5c7b8b0c38381dd38a74996fdf59441786bf6a33
aybeniz <aybenizkazimova15@gmail.com>
2024-09-25 02:49:12 +0400

    Modified AccountMapper to support new fields and features

--- src/main/java/az/edu/turing/mapper/AccountMapper.java
+++ src/main/java/az/edu/turing/mapper/AccountMapper.java
@@ -5,15 +5,12 @@ import az.edu.turing.model.dto.account.AccountDto;
 import az.edu.turing.model.dto.account.AccountResponse;
 import az.edu.turing.model.dto.account.AccountTransferRequest;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper
 public interface AccountMapper {
-//    AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
 
     AccountDto entityToDto(AccountEntity accountEntity);
 
